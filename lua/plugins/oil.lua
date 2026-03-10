4d2471563c938bf9a9352150a5fcb4df8e949a4b
Sławek <slawekzdunek2.3.6@gmail.com>
2026-03-10 23:05:27 +0100

    Vertical toggle term and oil remove keybinds

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,33 @@ return {
 	config = function()
 		require("oil").setup({
 			skip_confirm_for_simple_edits = false,
+			prompt_save_on_select_new_entry = false,
+			watch_for_changes = true,
+			view_options = {
+				show_hidden = false,
+				is_hidden_file = function(name, bufnr)
+					local m = name:match("^%.")
+					return m ~= nil
+				end,
+				natural_order = "fast",
+				case_insensitive = false,
+				sort = {
+					{ "type", "asc" },
+					{ "name", "asc" },
+				},
+				highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+					return nil
+				end,
+			},
+			keymaps = {
+				["<CR>"] = "actions.select",
+				["-"] = { "actions.parent", mode = "n" },
+				["_"] = { "actions.open_cwd", mode = "n" },
+				["gx"] = "actions.open_external",
+				["g."] = { "actions.toggle_hidden", mode = "n" },
+				["g\\"] = { "actions.toggle_trash", mode = "n" },
+			},
+			use_default_keymaps = false,
 		})
 		vim.keymap.set("n", "<leader>ff", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,9 +4,9 @@ return {
 		version = "*",
 		config = function()
 			require("toggleterm").setup({
-				size = 20, -- terminal height (for horizontal) or width (for vertical)
+				size = 80, -- terminal height (for horizontal) or width (for vertical)
 				open_mapping = [[<C-t>]], -- default toggle key
-				direction = "horizontal", -- "horizontal", "vertical", "tab", "float"
+				direction = "vertical", -- "horizontal", "vertical", "tab", "float"
 				close_on_exit = true,
 			})
 		end,
